1a2ad9523884a41d603eb7c184cf724fee9745ad
xu35244 <3154619782@qq.com>
2021-05-07 09:26:47 +0800

    '初始化'

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>
+        <Header />
+      </el-header>
+      <el-main>
+        <Main/>
+      </el-main>
+      <el-footer>
+        <Footer/>
+      </el-footer>
+    </el-container>
+    
+  </div>
+</template>
+
+<script>
+//1、导入vue子组件
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue"
+//2、注册vue子组件，components
+export default {
+  name: "App",
+  components: {
+    Header,
+    Main,
+    Footer
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+.el-header{
+  background-color: #244152;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="headerBook">
+     <div id="headerTitle">
+         <span>
+              {{message}}
+         </span>
+     </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"headerBook",
+    data(){
+        return {
+            message:"江西软件大学-新型图书管理系统"
+        }
+    }
+}
+</script>
+
+<style>
+#headerBook{
+    /* background-color: red; */
+}
+#headerTitle{
+    /* background-color: blue; */
+    width: 400px;
+    height: 60px;
+    color:white;
+    font-size: 20px;
+    font-weight: bolder;
+    line-height: 60px;
+}
+</style>
